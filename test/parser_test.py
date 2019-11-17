75d561d9d1852f33f527f2325885631c208d5999
cvoelcker (iliricon) <claas@voelcker.net>
2019-11-17 13:06:28 +0100

    Added metatuple for easier access

--- test/parser_test.py
+++ test/parser_test.py
@@ -4,9 +4,11 @@ from config_parser import config_parser
 parser = config_parser.ConfigGenerator('sample_data/example.yml')
 parsed = parser(argv[1:])
 print(parsed)
+print(parsed.PATHS.model_save_path)
 parser.dump_config('test/test.yml')
 
 parser = config_parser.ConfigGenerator('test/test.yml')
 parsed = parser(argv[1:])
 print(parsed)
+print(parsed.PATHS.model_save_path)
 parser.dump_config('test/test_2.yml')
